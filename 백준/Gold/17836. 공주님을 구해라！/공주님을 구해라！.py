37f69049f0dc70d0a92cf9aa2c22cab7c5d4ffda
sani0928 <kksan12@gmail.com>
2025-08-23 21:28:58 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 104 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.py
@@ -1,25 +1,28 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 N, M, T = map(int, input().split())
 
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-INF = float('inf')
+INF = 100**9
 dist = [[[INF] * 2 for _ in range(M)] for _ in range(N)]
 dist[0][0][0] = 0
 
 dr, dc = (0, 1, 0, -1), (1, 0, -1, 0)
 
-q = deque()
 # 순서대로 r, c, time, 명검 획득 여부
-q.append((0, 0, 0, 0))
+q = deque([(0, 0, 0, 0)])
 
 while q:
 
     cr, cc, time, get = q.popleft()
+
     # 시간 초과 하면 실패
     if time > T:
-        break
+        continue
     # 시간 내에 공주에게 도착 시 성공
     if cr == N - 1 and cc == M - 1:
         break
@@ -37,7 +40,7 @@ while q:
             if 0 <= nr < N and 0 <= nc < M and grid[nr][nc] != 1:
                 if time + 1 < dist[nr][nc][get]:
                     dist[nr][nc][get] = time + 1
-                    q.append((nr, nc, dist[nr][nc][get], 0))
+                    q.append((nr, nc, time + 1, 0))
     # 명검 있을 때 루트
     if get == 1:
         for k in range(4):
@@ -45,7 +48,8 @@ while q:
             if 0 <= nr < N and 0 <= nc < M:
                 if time + 1 < dist[nr][nc][get]:
                     dist[nr][nc][get] = time + 1
-                    q.append((nr, nc, dist[nr][nc][get], 1))
+                    q.append((nr, nc, time + 1, 1))
 
+# 두 루트 중 더 짧은 거
 ans = min(dist[N - 1][M - 1][0], dist[N - 1][M - 1][1])
 print(ans if ans <= T else 'Fail')
