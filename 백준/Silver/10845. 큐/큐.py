d8d8f2739955a17ca06cfb6075db0b8019380f05
sani0928 <kksan12@gmail.com>
2025-08-24 00:38:26 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+q = deque()
+ans = []
+
+for _ in range(N):
+    command = input().split()
+    if command[0] == 'push':
+        q.append(command[1])
+    elif command[0] == 'pop':
+        ans.append(q.popleft() if q else '-1')
+    elif command[0] == 'size':
+        ans.append(str(len(q)))
+    elif command[0] == 'empty':
+            ans.append('0' if q else '1')
+    elif command[0] == 'front':
+        ans.append(q[0] if q else '-1')
+    elif command[0] == 'back':
+        ans.append(q[-1] if q else '-1')
+
+print('\n'.join(ans))
