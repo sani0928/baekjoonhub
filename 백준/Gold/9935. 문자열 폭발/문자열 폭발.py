3ea7cf032fa013c7b43fd5894911bac9d4618d55
sani0928 <kksan12@gmail.com>
2025-10-09 01:12:44 +0900

    [Gold IV] Title: 문자열 폭발, Time: 600 ms, Memory: 50868 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,21 @@
+text = list(map(str, input().strip()))
+bomb_text = list(map(str, input().strip()))
+text_len = len(text)
+bomb_len = len(bomb_text)
+
+s = []
+
+for i in range(text_len):
+
+    s.append(text[i])
+    s_len = len(s)
+
+    if s_len >= bomb_len:
+        if s[s_len-bomb_len:s_len] == bomb_text:
+            for _ in range(bomb_len):
+                s.pop()
+
+if s:
+    print(''.join(s))
+else:
+    print('FRULA')
