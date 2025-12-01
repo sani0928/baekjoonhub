ef9b1e858e0c2bcc5140af4ee7366ecfe1fe731e
sani0928 <kksan12@gmail.com>
2025-12-01 12:47:42 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,17 @@
+T = int(input())
+for _ in range(T):
+    lst = list(map(str, input().strip()))
+    s = []; nope = False
+    for i in range(len(lst)):
+        if lst[i] == '(':
+            s.append('(')
+        else:
+            if not s:
+                nope = True
+                break
+            s.pop()
+    if nope or s:
+        print('NO')
+        continue
+    else:
+        print('YES')
