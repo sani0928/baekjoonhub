e0fe5c26e7f298a8fc1d84f18e1a921201a8320e
sani0928 <kksan12@gmail.com>
2025-12-01 11:15:08 +0900

    [Silver II] Title: 통계학, Time: 280 ms, Memory: 143028 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,20 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+N = int(input())
+nums = []
+for _ in range(N):
+    nums.append(int(input()))
+nums.sort()
+cnt = defaultdict(int)
+for num in nums:
+    cnt[num] += 1
+max_freq = max(cnt.values())
+candidate = [n for n in cnt if cnt[n] == max_freq]
+print(round(sum(nums) / N))
+print(nums[N//2])
+if len(candidate) == 1:
+    print(candidate[0])
+else:
+    print(candidate[1])
+print(nums[-1] - nums[0])
