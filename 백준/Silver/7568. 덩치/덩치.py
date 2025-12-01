c40ff2688997fd5f6f2e0013c5282dd4cee168cd
sani0928 <kksan12@gmail.com>
2025-12-01 12:39:27 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,17 @@
+N = int(input())
+people = []
+ans = [0] * N
+for idx in range(N):
+    weight, length = map(int, input().split())
+    people.append((idx, weight, length))
+
+for i, x, y in people:
+    rank = 0
+    for j in range(N):
+        if i == j:
+            continue
+        _, p, q = people[j]
+        if p > x and q > y:
+            rank += 1
+    ans[i] = rank + 1
+print(*ans)
