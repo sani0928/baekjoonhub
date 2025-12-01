786eeacff6c497541fb188f785ed2668aa5f2bd9
sani0928 <kksan12@gmail.com>
2025-12-01 09:34:47 +0900

    [Silver II] Title: 스택 수열, Time: 2572 ms, Memory: 35020 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,22 @@
+N = int(input())
+s = []
+cur = 0
+answer = []
+for _ in range(N):
+    num = int(input())
+
+    if cur < num:
+        for i in range(cur + 1, num + 1):
+            answer.append('+')
+            cur += 1
+            s.append(i)
+
+    if s[-1] == num:
+        answer.append('-')
+        s.pop()
+
+if s:
+    print('NO')
+else:
+    for ans in answer:
+        print(ans)
