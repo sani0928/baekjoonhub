9653e084224fa505c4642f27104b9d9bd9e51c10
sani0928 <kksan12@gmail.com>
2025-12-31 22:07:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 276 ms, Memory: 129488 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-prefix_sum = [[0] * (N + 1) for _ in range(N + 1)]
+def prefix_sum(lx, ly, rx, ry):
+    return prefix[rx][ry] - prefix[rx][ly-1] - prefix[lx-1][ry] + prefix[lx-1][ly-1]
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        prefix_sum[i][j] = matrix[i-1][j-1] + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1]
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+prefix = [[0] * (N+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        prefix[i][j] = grid[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
 
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    ans = prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
-    print(ans)
+    result = prefix_sum(x1, y1, x2, y2)
+    print(result)
