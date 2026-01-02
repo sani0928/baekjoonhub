4edf0b2ca34e88a6654e39c201128724486de5d9
sani0928 <kksan12@gmail.com>
2026-01-03 03:45:30 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 111224 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,29 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 M = int(input())
-S = tuple(map(str, input().strip()))
-
+S = list(input().rstrip())
 ans = 0
-
-sample = []
-for i in range(N*2+1):
+p_len = N+N+1
+mid = p_len//2
+p = []
+for i in range(p_len):
     if i % 2 == 0:
-        sample.append('I')
+        p.append('I')
     else:
-        sample.append('O')
+        p.append('O')
 
-for j in range(len(S)):
-    if S[j] == 'I':
-        idx = 0
-        result = True
-        while idx < N*2+1:
-            if j >= len(S) or S[j] != sample[idx]:
-                result = False
-                break
-
-            idx += 1
-            j += 1
-
-        if result:
-            ans += 1
-
+for j in range(mid, len(S) - mid):
+    if p == S[j-N:j+N+1]:
+        ans += 1
 print(ans)
