da0877c99c1d08406586b32acd29abdc74df0731
sani0928 <kksan12@gmail.com>
2026-01-13 23:31:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,17 +1,15 @@
 def solve():
     N = int(input())
-    ans = 0
-
-    def back(n):
-        nonlocal ans
-        if n == 0:
-            ans += 1
-            return
-        for i in range(1, 4):
-            if n - i >= 0:
-                back(n - i)
-    back(N)
-    print(ans)
+    dp = [0] * 12
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    if N <= 3:
+        print(dp[N])
+    else:
+        for i in range(4, N + 1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        print(dp[N])
 
 if __name__ == '__main__':
     T = int(input())
