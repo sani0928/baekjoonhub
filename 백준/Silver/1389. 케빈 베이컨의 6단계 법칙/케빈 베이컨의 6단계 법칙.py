6d41cf2d4ba527d24635c01072edf76c304d0c9f
sani0928 <kksan12@gmail.com>
2026-01-16 06:07:28 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -3,33 +3,49 @@ from collections import deque
 input = sys.stdin.readline
 
 def solve():
-    N, M = map(int, input().split())
-    graph = [[] for _ in range(N + 1)]
-    ans = None
-    total_sum = 10**9
-    for _ in range(M):
+
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
         p1, p2 = map(int, input().split())
         graph[p1].append(p2)
         graph[p2].append(p1)
+    kevin_bacon = [0] * (n + 1)
 
-    for p in range(1, N + 1):
-        kevin_bacon = 0
-        q = deque([(p, 0)])
-        vis, check = [0] * (N + 1), [0] * (N + 1)
-        vis[p], check[p] = 1, 1
+    def bfs(start):
+        nonlocal kevin_bacon
+
+        ans = None
+        total_sum = 10 ** 9
+        q = deque([(start, 0, start)])
+        vis, check = [[0] * (n + 1) for _ in range(n + 1)], [[0] * (n + 1) for _ in range(n + 1)]
+        vis[start][start], check[start][start] = 1, 1
+
         while q:
-            cur, dist = q.popleft()
-            if not check[cur]:
-                kevin_bacon += dist
+            cur, dist, target = q.popleft()
+
+            if cur != target and not vis[cur][cur]:
+                vis[cur][cur] = 1
+                q.append((cur, 0, cur))
+
+            if not check[target][cur]:
+                kevin_bacon[target] += dist
+
             for nx in graph[cur]:
-                if vis[nx]:
+                if vis[target][nx]:
                     continue
-                vis[nx] = 1
-                q.append((nx, dist + 1))
-        if total_sum > kevin_bacon:
-            total_sum = kevin_bacon
-            ans = p
-    print(ans)
+                vis[target][nx] = 1
+                q.append((nx, dist + 1, target))
+
+        for i in range(1, n + 1):
+            if total_sum <= kevin_bacon[i]:
+                continue
+            total_sum = kevin_bacon[i]
+            ans = i
+            
+        return ans
+    print(bfs(1))
 
 if __name__ == '__main__':
     solve()
