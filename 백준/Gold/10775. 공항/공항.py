bf3d1292ec395db701ff0bf5d2e1f4c067c39ee9
sani0928 <kksan12@gmail.com>
2026-01-17 08:11:17 +0900

    [Gold II] Title: 공항, Time: 112 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -1,40 +1,37 @@
 import sys
 input = sys.stdin.readline
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a < b:
-        parent[b] = a
+def solve():
+    def union(a, b):
+        a = find(a)
+        b = find(b)
+        if a < b:
+            parent[b] = a
+            return
+        parent[a] = b
         return
-    parent[a] = b
-    return
 
-def find(x):
-    if x != parent[x]:
-        parent[x] = find(parent[x])
-    return parent[x]
+    def find(x):
+        if x != parent[x]:
+            parent[x] = find(parent[x])
+        return parent[x]
 
-def docking(max_g):
-    global ans, docked
-
-    max_g = find(max_g)
-    for g in range(max_g, 0, -1):
-        if g in docked:
-            continue
-        docked.add(g)
-        # 한칸 앞으로
-        union(g, g - 1)
+    def docking(gate):
+        nonlocal ans
+        gate = find(gate)
+        if gate == 0:
+            return 0
+        union(gate, gate - 1)
         ans += 1
         return 1
-    return 0
 
-g = int(input())
-ans = 0
-docked = set()
-parent = list(range(g + 1))
-for _ in range(int(input())):
-    suc = docking(int(input()))
-    if not suc:
-        break
-print(ans)
+    ans = 0
+    parent = list(range(int(input()) + 1))
+    for _ in range(int(input())):
+        suc = docking(int(input()))
+        if not suc:
+            return ans
+    return ans
+
+if __name__ == '__main__':
+    print(solve())
