31ac5c07f4f233e6bb25806cbf3ce056cfebc164
sani0928 <kksan12@gmail.com>
2026-01-17 06:58:22 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 456 ms, Memory: 79628 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.py
@@ -1,39 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-def search(cr, cc, num):
-    global ans
+def solve():
+    def next_dir(r, c, cmd):
+        if cmd == 'U':
+            return r + dr[0], c + dc[0]
+        elif cmd == 'D':
+            return r + dr[1], c + dc[1]
+        elif cmd == 'L':
+            return r + dr[2], c + dc[2]
+        return r + dr[3], c + dc[3]
 
-    if g[cr][cc] == 'U':
-        nr, nc = cr + dr[0], cc + dc[0]
-    elif g[cr][cc] == 'D':
-        nr, nc = cr + dr[1], cc + dc[1]
-    elif g[cr][cc] == 'L':
-        nr, nc = cr + dr[2], cc + dc[2]
-    else:
-        nr, nc = cr + dr[3], cc + dc[3]
+    def search(cr, cc, num):
+        nonlocal ans
 
-    if check[nr][nc]:
-        if check[nr][nc] == num:
-            ans += 1
+        nr, nc = next_dir(cr, cc, g[cr][cc])
+        if check[nr][nc]:
+            if check[nr][nc] == num:
+                ans += 1
             return
+        if not check[nr][nc]:
+            check[nr][nc] = num
+            search(nr, nc, num)
         return
 
-    if not check[nr][nc]:
-        check[nr][nc] = num
-        search(nr, nc, num)
-    return
-# 상하좌우
-dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
-n, m = map(int, input().split())
-g = [list(map(str, input().rstrip())) for _ in range(n)]
-ans, turn = 0, 1
-check = [[0] * m for _ in range(n)]
-for r in range(n):
-    for c in range(m):
-        if check[r][c] != 0:
-            continue
-        check[r][c] = turn
-        search(r, c, turn)
-        turn += 1
-print(ans)
+    dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
+    n, m = map(int, input().split())
+    g = [list(map(str, input().rstrip())) for _ in range(n)]
+    ans, turn = 0, 1
+    check = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if check[i][j] != 0:
+                continue
+            check[i][j] = turn
+            search(i, j, turn)
+            turn += 1
+    return ans
+
+if __name__ == '__main__':
+    print(solve())
