9dd769f6557255d35a574382625e2fc42938d1bb
sani0928 <kksan12@gmail.com>
2026-01-17 02:54:11 +0900

    [Platinum V] Title: 행성 터널, Time: 916 ms, Memory: 102548 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,44 +1,47 @@
 import sys
 input = sys.stdin.readline
 
-def union(x, y):
-    x = find(x)
-    y = find(y)
-    if x != y:
-        if x < y:
-            parent[y] = x
-        else:
-            parent[x] = y
-    return
+def solve():
+    def union(x, y):
+        x, y = find(x), find(y)
+        if x == y:
+            return 0
+        if size[x] < size[y]:
+            x, y = y, x
+        size[x] += size[y]
+        parent[y] = x
+        return 1
 
-def find(x):
-    if x != parent[x]:
-        return find(parent[x])
-    return x
+    def find(x):
+        if x != parent[x]:
+            parent[x] = find(parent[x])
+        return parent[x]
 
-def add_edges(coord):
+    def add_candi():
+        lst = []
+        for xyz in range(3):
+            edges = [(nodes[i][xyz], i) for i in range(1, N + 1)]
+            edges.sort()
+            for j in range(1, N):
+                c1, n1 = edges[j]
+                c2, n2 = edges[j - 1]
+                lst.append((abs(c1 - c2), n1, n2))
+        lst.sort()
+        return lst
 
-    lst = []
-    for i in range(1, N + 1):
-        lst.append((nodes[i][coord], i))
-    lst.sort()
-    for i in range(1, N):
-        candi.append((abs(lst[i][0] - lst[i - 1][0]), lst[i][1], lst[i - 1][1]))
-    return lst
+    N = int(input())
+    nodes = [(0, 0, 0)] + [tuple(map(int, input().split())) for _ in range(N)]
+    parent = list(range(N + 1))
+    candi = add_candi()
+    size = [1] * (N + 1)
+    ans, cnt = 0, 0
+    for cost, a, b in candi:
+        if union(a, b):
+            ans += cost
+            cnt += 1
+            if cnt == N - 1:
+                break
+    return ans
 
-N = int(input())
-nodes = [(0, 0, 0)] + [tuple(map(int, input().split())) for _ in range(N)]
-parent = list(range(N + 1))
-candi = []
-x_edges, y_edges, z_edges = add_edges(0), add_edges(1), add_edges(2)
-candi.sort()
-ans, cnt = 0, 0
-while cnt < N - 1:
-    for i in range(len(candi)):
-        cost, a, b = candi[i]
-        if find(a) == find(b):
-            continue
-        union(a, b)
-        ans += cost
-        cnt += 1
-print(ans)
+if __name__ == '__main__':
+    print(solve())
