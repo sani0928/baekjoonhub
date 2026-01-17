ce0ef9fb6a050a8a230a010cd7d226ef5374e589
sani0928 <kksan12@gmail.com>
2026-01-17 19:56:12 +0900

    [Gold IV] Title: 거짓말, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,38 @@
+def solve():
+
+    def torf(pt, l):
+        nonlocal ans
+        idx = 0
+        while idx < l:
+            if t[pt[idx]]:
+                ans -= 1
+                return
+            idx += 1
+        return
+
+    ans = M
+    t = [0] * (N + 1)
+    _, *know = list((map(int, input().split())))
+    for i in know:
+        t[i] = 1
+    parties = []
+    for _ in range(M):
+        _, *people = list(map(int, input().split()))
+        parties.append(people)
+    while True:
+        newt = t.copy()
+        for party in parties:
+            for p in party:
+                if t[p]:
+                    for p2 in party:
+                        t[p2] = 1
+        if newt == t:
+            break
+            
+    for i in range(M):
+        torf(parties[i], len(parties[i]))
+    return ans
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    print(solve())
