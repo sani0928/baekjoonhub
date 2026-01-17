e83f7be7107688a17e1550192745b4fbd87363d7
sani0928 <kksan12@gmail.com>
2026-01-18 02:52:06 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 692 ms, Memory: 40036 KB -BaekjoonHub

---
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def heart():
+    for j in range(n):
+        for i in range(n):
+            check = 0
+            for k in range(4):
+                ni, nj = i + dr[k], j + dc[k]
+                if 0 > ni or n <= ni or 0 > nj or n <= nj:
+                    continue
+                if board[ni][nj] == '*':
+                    check += 1
+            if check == 4:
+                return i, j
+
+def length(sr, sc, d, again):
+    l = 0
+    r, c = sr + dr[d], sc + dc[d]
+    while board[r][c] == '*':
+        l += 1
+        if 0 > r + dr[d] or n <= r + dr[d] or 0 > c + dc[d] or n <= c + dc[d]:
+            break
+        if board[r + dr[d]][c + dc[d]] != '*':
+            break
+        r, c = r + dr[d], c + dc[d]
+    if again:
+        left = length(r, c - 1, 1, 0)
+        right = length(r, c + 1, 1, 0)
+        return l, left, right
+    return l
+
+n = int(input())
+board = [list(map(str, input().rstrip())) for _ in range(n)]
+dr, dc = (-1, 1, 0, 0), (0, 0, -1, 1)
+heart_r, heart_c = heart()
+print(heart_r + 1, heart_c + 1)
+ans1 = length(heart_r, heart_c, 2, 0)
+ans2 = length(heart_r, heart_c, 3, 0)
+ans3, ans4, ans5 = length(heart_r, heart_c, 1, 1)
+print(ans1, ans2, ans3, ans4, ans5)
