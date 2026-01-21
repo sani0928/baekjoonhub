5b4ab55993cd6fcad3f2410ca8ee9675cc5a3da2
sani0928 <kksan12@gmail.com>
2026-01-22 05:30:58 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 2520 ms, Memory: 43680 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,12 @@
+def full():
+    if g == 'Y':
+        return 1
+    elif g == 'F':
+        return 2
+    else:
+        return 3
+n, g = map(str, input().split())
+n, required, players = int(n), full(), set()
+for _ in range(n):
+    players.add(input().rstrip())
+print(len(players) // required)
