eed3ea8267f2b544625e1cc11f4b4d5633b837d8
sani0928 <kksan12@gmail.com>
2026-01-26 03:26:54 +0900

    [Silver V] Title: 문서 검색, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -3,17 +3,14 @@ char = input()
 dl, cl = len(docu), len(char)
 cnt, cur = 0, 0
 while cur <= dl - cl:
-    if docu[cur] == char[0]:
-        i = 0
-        l = cur
-        while i < cl and l < dl and docu[l] == char[i]:
-            l += 1
-            i += 1
-        if i == cl:
-            cnt += 1
-            cur = l
-        else:
-            cur += 1
-        continue
-    cur += 1
+    i = 0
+    l = cur
+    while i < cl and l < dl and docu[l] == char[i]:
+        l += 1
+        i += 1
+    if i == cl:
+        cnt += 1
+        cur = l
+    else:
+        cur += 1
 print(cnt)
