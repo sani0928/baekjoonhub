97934facadbbe6944df029d37f6f2ff0bc6a61b3
sani0928 <kksan12@gmail.com>
2026-01-27 05:56:50 +0900

    [Silver IV] Title: 알바생 강호, Time: 2464 ms, Memory: 37048 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -0,0 +1,9 @@
+n = int(input())
+line = sorted([int(input()) for _ in range(n)], reverse=True)
+order, ans = 0, 0
+i = 0
+while i < n and line[i] - order > 0:
+    ans += line[i] - order
+    order += 1
+    i += 1
+print(ans)
