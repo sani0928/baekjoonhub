43c87aa1c35fb547ea6b8467e4caa1da3563b090
sani0928 <kksan12@gmail.com>
2026-02-02 00:11:13 +0900

    [Silver V] Title: 폴리오미노, Time: 84 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.js
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.js
@@ -1,37 +1,9 @@
 const filePath = process.platform === "linux" ? '/dev/stdin' : '1343.txt';
-const board = require('fs').readFileSync(filePath, 'utf8').trim().split('').concat(['.']);
+let board = require('fs').readFileSync(filePath, 'utf8').toString().trim();
 
-let s = 0;
-let success = true;
-
-for (let i = 0; i < board.length; i++) {
-    if (s == 4) {
-        for (let j = i - 4; j < i; j++) {
-            board[j] = 'A'
-        };
-        s = 0
-    };
-    if (board[i] == 'X') {
-        s += 1
-    } else {
-        if (s == 4) {
-            for (let j = i - 4; j < i; j++) {
-                board[j] = 'A'
-            }
-        } else if (s == 2) {
-            for (let j = i - 2; j < i; j++) {
-                board[j] = 'B'
-            }
-        } else if (s == 1 || s == 3) {
-            success = false;
-            break;
-        }
-        s = 0
-    };
-};
-
-if (success) {
-    console.log(board.slice(0, -1).join(''))
-} else {
+board = board.replaceAll('XXXX', 'AAAA').replaceAll('XX', 'BB');
+if (board.includes('X')) {
     console.log(-1)
+} else {
+    console.log(board)    
 };
