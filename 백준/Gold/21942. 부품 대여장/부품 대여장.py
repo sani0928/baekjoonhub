cb8cf2be631ade371b9db3b082ec1901344488a5
sani0928 <kksan12@gmail.com>
2026-02-02 22:35:38 +0900

    [Gold II] Title: 부품 대여장, Time: 376 ms, Memory: 43984 KB -BaekjoonHub

--- 백준/Gold/21942. 부품 대여장/부품 대여장.py
+++ 백준/Gold/21942. 부품 대여장/부품 대여장.py
@@ -1,31 +1,37 @@
+import sys
 from datetime import datetime
+input = sys.stdin.readline
+
+def check(current, before):
+    total = ((current - before).days * 1440) + ((current - before).seconds // 60)
+    diff = total - limit
+    # 기간 초과
+    if diff > 0:
+        if flist.get(nick):
+            flist[nick] += diff * F
+            return
+        flist[nick] = diff * F
+    del note[(nick, item)]
 
 N, L, F = input().split()
 N, F = int(N), int(F)
 # 분 기준
-limit = int(L[:3]) * 1440 + int(L[4:6]) * 60 + int(L[7:9])
+l_day, l_time = L.split('/')
+l_h, l_m = map(int, l_time.split(':'))
+limit = int(l_day) * 1440 + l_h * 60 + l_m
 
 flist, note = {}, {}
 for _ in range(N):
-    date, t, item, nick = input().split()
-    month, day = int(date.split('-')[1]), int(date.split('-')[2])
-    hour, minute = int(t.split(':')[0]), int(t.split(':')[1])
+    date, time, item, nick = input().split()
+    _, month, day = map(int, date.split('-'))
+    hour, minute = map(int, time.split(':'))
     now = datetime(2021, month, day, hour, minute)
-    # 대여하러 옴
+    # 대여
     if not note.get((nick, item)):
         note[(nick, item)] = now
         continue
-
-    before = note[(nick, item)]
-    total = ((now - before).days * 1440) + ((now - before).seconds // 60)
-    # 늦어서 벌금
-    diff = total - limit
-    if diff > 0:
-        if flist.get(nick):
-            flist[nick] += diff * F
-            continue
-        flist[nick] = diff * F
-    del note[(nick, item)]
+    # 반납
+    check(now, note[nick, item])
 
 if flist:
     for k in sorted(flist):
