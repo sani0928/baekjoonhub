b51abddd185288b65e3b444b8c468c2d7d4406eb
sani0928 <kksan12@gmail.com>
2026-02-03 20:26:25 +0900

    [Gold IV] Title: 로하의 농사, Time: 936 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/26153. 로하의 농사/로하의 농사.py
+++ 백준/Gold/26153. 로하의 농사/로하의 농사.py
@@ -5,28 +5,27 @@ def solve():
     def search(cx, cy, rest, total, pipe):
         nonlocal ans
 
-        for dx, dy in pipe:
-            nx, ny = cx + dx, cy + dy
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if v[nx][ny]:
-                continue
-            if rest >= 1:
-                v[nx][ny] = 1
-                search(nx, ny, rest - 1, total + board[nx][ny], [(dx, dy)])
-                v[nx][ny] = 0
-            if rest >= 2:
-                if dx == 0:
+        if rest > 0:
+            for dx, dy in pipe:
+                nx, ny = cx + dx, cy + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if v[nx][ny]:
+                    continue
+                if rest >= 1:
                     v[nx][ny] = 1
-                    search(nx, ny, rest - 2, total + board[nx][ny], [(-1, 0), (1, 0)])
+                    search(nx, ny, rest - 1, total + board[nx][ny], [(dx, dy)])
                     v[nx][ny] = 0
-                else:
-                    v[nx][ny] = 1
-                    search(nx, ny, rest - 2, total + board[nx][ny], [(0, -1), (0, 1)])
-                    v[nx][ny] = 0
-
+                if rest >= 2:
+                    if dx == 0:
+                        v[nx][ny] = 1
+                        search(nx, ny, rest - 2, total + board[nx][ny], [(-1, 0), (1, 0)])
+                        v[nx][ny] = 0
+                    else:
+                        v[nx][ny] = 1
+                        search(nx, ny, rest - 2, total + board[nx][ny], [(0, -1), (0, 1)])
+                        v[nx][ny] = 0
         ans = max(ans, total)
-        return
 
     n, m = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(n)]
