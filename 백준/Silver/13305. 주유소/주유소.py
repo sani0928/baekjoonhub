8347487e495eed2fcbd0489d987c0cb82232100b
sani0928 <kksan12@gmail.com>
2026-02-09 07:10:43 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,9 +4,9 @@ N = int(input())
 dist = list(map(int, input().split()))
 city = list(map(int, input().split()))
 ans = 0
+mn_cost = 10 ** 9
 for i in range(N - 1):
-    mn = 10 ** 9
-    for j in range(i + 1):
-        mn = min(mn, dist[i] * city[j])
-    ans += mn
+    if mn_cost > city[i]:
+        mn_cost = city[i]
+    ans += mn_cost * dist[i]
 print(ans)
