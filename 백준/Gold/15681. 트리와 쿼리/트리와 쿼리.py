851a9e8c930c2abf861a343e4a3e5c7ce19bc43f
sani0928 <kksan12@gmail.com>
2026-02-11 13:16:27 +0900

    [Gold V] Title: 트리와 쿼리, Time: 384 ms, Memory: 68568 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,36 +1,24 @@
 import sys; sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-def maketree(root):
-    t = [[] for _ in range(n + 1)]
-    s = [root]
-    vis = [0] * (n + 1)
-    vis[root] = 1
-    while s:
-        cur = s.pop()
-        for nx in graph[cur]:
-            if vis[nx]:
-                continue
-            vis[nx] = 1
-            t[cur].append(nx)
-            s.append(nx)
-    return t
-
-def cal_cnt(cur):
+def dfs(cur):
     cnt = 1
     for nx in tree[cur]:
-        cnt += cal_cnt(nx)
-    sub[cur] = cnt
-    return sub[cur]
+        if parent[cur] == nx:
+            continue
+        parent[nx] = cur
+        cnt += dfs(nx)
+    sub_cnt[cur] = cnt
+    return sub_cnt[cur]
 
-n, r, q = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-for _ in range(n - 1):
+N, R, Q = map(int, input().split())
+tree = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-tree = maketree(r)
-sub = [0] * (n + 1)
-cal_cnt(r)
-for _ in range(q):
-    print(sub[int(input())])
+    tree[u].append(v)
+    tree[v].append(u)
+sub_cnt = [1] * (N + 1)
+parent = [-1] * (N + 1)
+parent[R] = 0
+dfs(R)
+print(*(sub_cnt[int(input())] for _ in range(Q)), sep='\n')
