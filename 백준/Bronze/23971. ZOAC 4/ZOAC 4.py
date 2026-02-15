1a96b88c87bc000c7238a383d50f9ff4700ed372
sani0928 <kksan12@gmail.com>
2026-02-15 20:42:07 +0900

    [Bronze III] Title: ZOAC 4, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -1,12 +1,5 @@
-def calcul(l, d):
-    if l - 1 >= d + 1:
-        return (l - 1) // (d + 1) + 1
-    return 0
-
+from math import ceil
 h, w, n, m = map(int, input().split())
-h_cnt, w_cnt = calcul(h, n), calcul(w, m)
-if w_cnt == 0 or h_cnt == 0:
-    if w_cnt == 0 and h_cnt != 0: print(h_cnt)
-    elif w_cnt != 0 and h_cnt == 0: print(w_cnt)
-    else: print(1)
-else: print(h_cnt * w_cnt)
+h_cnt = ceil(h / (n + 1))
+w_cnt = ceil(w / (m + 1))
+print(h_cnt * w_cnt)
