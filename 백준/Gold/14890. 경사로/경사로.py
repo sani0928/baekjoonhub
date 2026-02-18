b657731920e8c66c7486620adc7c0657dfbaf561
sani0928 <kksan12@gmail.com>
2026-02-18 16:47:24 +0900

    [Gold III] Title: 경사로, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -0,0 +1,36 @@
+def solve():
+    def search(matrix):
+        used = [0] * n
+        for i in range(1, n):
+            diff = matrix[i - 1] - matrix[i]
+            if diff == 0:
+                continue
+            if abs(diff) > 1:
+                return 0
+            if diff == 1: # matrix[i - 1] > matrix[i]
+                cur = matrix[i]
+                for j in range(i, i + l):
+                    if j >= n or cur != matrix[j] or used[j]:
+                        return 0
+                for j in range(i, i + l):
+                    used[j] = 1
+            else: # matrix[i - 1] < matrix[i]
+                cur = matrix[i - 1]
+                for j in range(i - l, i):
+                    if j < 0 or cur != matrix[j] or used[j]:
+                        return 0
+                for j in range(i - l, i):
+                    used[j] = 1
+        return 1
+
+    n, l = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    board_reverse = [[board[x][y] for x in range(n)] for y in range(n)]
+    ans = 0
+    for r in range(n):
+        ans += search(board[r])
+        ans += search(board_reverse[r])
+    print(ans)
+
+if __name__ == '__main__':
+    solve()
