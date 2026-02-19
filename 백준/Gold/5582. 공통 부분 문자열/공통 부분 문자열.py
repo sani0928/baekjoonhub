2eedbacb48dc82d91d51adc69c2c9644c2aa8071
sani0928 <kksan12@gmail.com>
2026-02-19 18:39:27 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 2156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -1,20 +1,23 @@
 import sys
 input = sys.stdin.readline
-s1, s2 = input().rstrip(), input().rstrip()
-if len(s1) > len(s2):
-    lg, sht = s1, s2
-else:
-    lg, sht = s2, s1
-rec = [0] * (len(sht) + 1)
-mx = 0
-for ch in lg:
-    keep = 0
-    for i in range(1, len(sht) + 1):
-        last = rec[i]
-        if ch == sht[i - 1]:
-            rec[i] = keep + 1
-            mx = max(mx, rec[i])
-        else:
-            rec[i] = 0
-        keep = last
-print(mx)
+def solve():
+    s1, s2 = input().rstrip(), input().rstrip()
+    if len(s1) > len(s2):
+        lg, sht = s1, s2
+    else:
+        lg, sht = s2, s1
+    rec = [0] * (len(sht) + 1)
+    mx = 0
+    for ch in lg:
+        keep = 0
+        for i in range(1, len(sht) + 1):
+            last = rec[i]
+            if ch == sht[i - 1]:
+                rec[i] = keep + 1
+                mx = max(mx, rec[i])
+            else:
+                rec[i] = 0
+            keep = last
+    return mx
+if __name__ == '__main__':
+    print(solve())
