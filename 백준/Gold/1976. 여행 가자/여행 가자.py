2c704bdea73633c1eda928015b150b5596de0a30
sani0928 <kksan12@gmail.com>
2026-02-23 19:04:45 +0900

    [Gold IV] Title: 여행 가자, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,6 +1,4 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+import sys; input = sys.stdin.readline
 
 n, m = int(input()), int(input())
 road = [[] for _ in range(n + 1)]
@@ -12,14 +10,14 @@ for i in range(1, n + 1):
 schedule = tuple(map(int, input().split()))
 vis = [0] * (n + 1)
 vis[schedule[0]] = 1
-q = deque([schedule[0]])
-while q:
-    cur = q.popleft()
+s = [schedule[0]]
+while s:
+    cur = s.pop()  
     for nx in road[cur]:
         if vis[nx]:
             continue
         vis[nx] = 1
-        q.append(nx)
+        s.append(nx)
 ans = 0
 for city in schedule:
     if vis[city] == 1:
