2b2f1b28644a5687c84b33a3f7877fb00efce13c
sani0928 <kksan12@gmail.com>
2026-02-25 02:29:44 +0900

    [Silver III] Title: 파일 정리, Time: 192 ms, Memory: 43332 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,9 +1,9 @@
-from collections import defaultdict
-
+import sys; from collections import defaultdict
+input = sys.stdin.readline
 n = int(input())
 arrange = defaultdict(int)
 for _ in range(n):
-    name = input().split('.')[1]
+    name = input().rstrip().split('.')[1]
     arrange[name] += 1
 for file in sorted(arrange):
     print(file, arrange[file])
