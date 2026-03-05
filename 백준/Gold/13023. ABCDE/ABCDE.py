20a4660793dfc045a8d70f4213d772c1f35cd41e
sani0928 <kksan12@gmail.com>
2026-03-06 00:26:50 +0900

    [Gold V] Title: ABCDE, Time: 656 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    def recur(cur, dist):
+        nonlocal end
+        if end:
+            return
+        if dist == 4:
+            end = True
+            return
+        for nx in graph[cur]:
+            if not v[nx]:
+                v[nx] = 1
+                recur(nx, dist + 1)
+                v[nx] = 0
+
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n)]
+    v = [0] * n
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    end = False
+    for i in range(n):
+        if end:
+            return 1
+        v[i] = 1
+        recur(i, 0)
+        v[i] = 0
+    return 0
+
+if __name__ == '__main__':
+    print(solve())
