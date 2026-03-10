eba5047ec8684e08df3d2ec3d6e8be264adc8d45
sani0928 <kksan12@gmail.com>
2026-03-10 23:17:05 +0900

    [Silver V] Title: 막대기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -2,14 +2,14 @@ def solve():
     x = int(input())
     if x == 64:
         return 1
-    candi = [32, 16, 8, 4, 2, 1]
+    candi = 32
+    total = 0
     ans = 0
-    for i in range(6):
-        if x == 0:
-            return ans
-        if x - candi[i] >= 0:
-            x -= candi[i]
+    while total != x:
+        if total + candi <= x:
+            total += candi
             ans += 1
+        candi //= 2
     return ans
 
 if __name__ == '__main__':
