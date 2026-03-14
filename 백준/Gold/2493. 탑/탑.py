9348afd653c54d6c5e7f2110aee99aceea086233
sani0928 <kksan12@gmail.com>
2026-03-15 01:04:34 +0900

    [Gold V] Title: 탑, Time: 584 ms, Memory: 133644 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,16 +1,13 @@
 n = int(input())
-arr = [0] + list(map(int, input().split()))
-ans = [0] * (n + 1)
-s = [(arr[1], 1)]
-for i in range(2, n + 1):
-    if s[-1][0] < arr[i]:
-        while s and s[-1][0] < arr[i]:
-            s.pop()
-        if not s:
-            ans[i] = 0
-        else:
-            ans[i] = s[-1][1]
+arr = list(map(int, input().split()))
+ans = [0] * n
+s = [(arr[0], 0)]
+for i in range(1, n):
+    while s and s[-1][0] < arr[i]:
+        s.pop()
+    if not s:
+        ans[i] = 0
     else:
-        ans[i] = s[-1][1]
+        ans[i] = s[-1][1] + 1
     s.append((arr[i], i))
-print(*ans[1:])
+print(*ans)
