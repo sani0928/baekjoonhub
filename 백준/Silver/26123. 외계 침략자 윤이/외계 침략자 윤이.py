a8cbec5ed7415745c365ce1d2ffa1aac788cc7ee
sani0928 <kksan12@gmail.com>
2026-03-22 10:05:08 +0900

    [Silver IV] Title: 외계 침략자 윤이, Time: 216 ms, Memory: 62196 KB -BaekjoonHub

--- 백준/Silver/26123. 외계 침략자 윤이/외계 침략자 윤이.py
+++ 백준/Silver/26123. 외계 침략자 윤이/외계 침략자 윤이.py
@@ -1,14 +1,12 @@
+def calcul():
+    cnt, i = 0, 0
+    while i < n and max_h - arr[i] < d:
+        cnt += arr[i] + d - max_h
+        i += 1
+    return cnt
+
 n, d = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort(reverse=True)
 max_h = arr[0]
-cnt = 0
-if max_h >= d:
-    for i in range(n):
-        if max_h - arr[i] < d:
-            cnt += arr[i] + d - max_h
-        else:
-            break
-    print(cnt)
-else:
-    print(sum(arr))
+print(calcul() if max_h >= d else sum(arr))
