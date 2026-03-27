19b608e2eb68f3dbecaed9ceee820a20f6ccbd41
sani0928 <kksan12@gmail.com>
2026-03-27 14:47:58 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 3824 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,35 +1,33 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
 n, k = map(int, input().split())
-belt = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+belt = deque([i, 0] for i in arr)
 LEN = n * 2
-pos = [0] * LEN
 cnt = 0
 turn = 0
 while cnt < k:
     # 1-1) 벨트 이동
-    belt.insert(0, belt.pop())
-    pos.insert(0, pos.pop())
+    belt.appendleft(belt.pop())
     # 1-2) 내리는 위치에 로봇 있으면 내림
-    if pos[n-1]:
-        pos[n-1] = 0
+    if belt[n-1][1]:
+        belt[n-1][1] = 0
     # 2) 순서대로 벨트 위 로봇 이동
     for i in range(n - 2, -1, -1):
-        if not pos[i]:
+        if not belt[i][1]:
             continue
         nx = i + 1
-        if not pos[nx] and belt[nx] >= 1:
-                pos[i] = 0
-                pos[nx] = 1 if nx != n-1 else 0
-                belt[nx] -= 1
-                if belt[nx] == 0:
+        if not belt[nx][1] and belt[nx][0] >= 1:
+                belt[i][1] = 0
+                belt[nx][1] = 1 if nx != n-1 else 0
+                belt[nx][0] -= 1
+                if belt[nx][0] == 0:
                     cnt += 1
     # 3) 올리는 위치에 로봇 올리기
-    if belt[0] >= 1:
-        pos[0] = 1
-        belt[0] -= 1
-        if belt[0] == 0:
+    if belt[0][0] >= 1:
+        belt[0][1] = 1
+        belt[0][0] -= 1
+        if belt[0][0] == 0:
             cnt += 1
     turn += 1
 
