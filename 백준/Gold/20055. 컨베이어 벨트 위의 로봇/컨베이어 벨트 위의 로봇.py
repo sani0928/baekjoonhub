6080ffdbfed4246c7f66d9525f0159be254644dd
sani0928 <kksan12@gmail.com>
2026-03-27 14:39:41 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2592 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
 belt = list(map(int, input().split()))
 LEN = n * 2
@@ -11,20 +14,17 @@ while cnt < k:
     # 1-2) 내리는 위치에 로봇 있으면 내림
     if pos[n-1]:
         pos[n-1] = 0
-    # 2-1) 순서대로 벨트 위 로봇 이동
+    # 2) 순서대로 벨트 위 로봇 이동
     for i in range(n - 2, -1, -1):
         if not pos[i]:
             continue
         nx = i + 1
         if not pos[nx] and belt[nx] >= 1:
                 pos[i] = 0
-                pos[nx] = 1
+                pos[nx] = 1 if nx != n-1 else 0
                 belt[nx] -= 1
                 if belt[nx] == 0:
                     cnt += 1
-    # 2-2) 내리는 위치에 로봇 있으면 내림
-    if pos[n-1]:
-        pos[n-1] = 0
     # 3) 올리는 위치에 로봇 올리기
     if belt[0] >= 1:
         pos[0] = 1
