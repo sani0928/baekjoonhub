0d2de9d2e4cb2870729413211413cc091bff2fd1
sani0928 <kksan12@gmail.com>
2026-03-28 18:06:19 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 752 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-dr, dc = (0, 1, 0, -1), (-1, 0, 1, 0)
+dv = [(0, 1, 0, -1), (-1, 0, 1, 0)]
 left = [
     (-1, 0, 0.07), (1, 0, 0.07),
     (-2, 0, 0.02), (2, 0, 0.02),
@@ -12,7 +12,7 @@ left = [
 down = [(-y, x, radio) for x, y, radio in left]
 right = [(x, -y, radio) for x, y, radio in left]
 up = [(y, -x, radio) for x, y, radio in left]
-move = [left, down, right, up]
+pos = [left, down, right, up]
 
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
@@ -26,9 +26,11 @@ def tornado():
         for _ in range(2):
             for _ in range(l):
                 # 다음 이동 위치로 갱신
-                cr, cc = cr + dr[d], cc + dc[d]
-                # 범위 밖 모래 갱신
+                cr, cc = cr + dv[0][d], cc + dv[1][d]
+                # splash 및 범위 밖 모래 계산 갱신
                 ans += splash(cr, cc, d)
+                # 모래를 다 흩날린 후 해당 위치 모래 비우기
+                board[cr][cc] = 0
                 if cr == 0 and cc == 0:
                     return ans
             d = (d + 1) % 4
@@ -37,21 +39,19 @@ def tornado():
 def splash(r, c, d):
     if not board[r][c]:
         return 0
-    rest = board[r][c]
+    rest = total = board[r][c]
     out = 0
-    for x, y, radio in move[d]:
-        sand = int(board[r][c] * radio)
+    for x, y, radio in pos[d]:
         nr, nc = r + x, c + y
+        sand = int(total * radio)
         rest -= sand
         if 0 > nr or n <= nr or 0 > nc or n <= nc:
             out += sand
             continue
         # 비율에 맞게 모래 splash
         board[nr][nc] += sand
-    # 모래를 다 흩날린 후 해당 위치 모래 비우기
-    board[r][c] = 0
     # 남은 모래 알파 자리에 추가 (알파 자리가 범위 밖이면 out + rest)
-    ax, ay = r + dr[d], c + dc[d]
+    ax, ay = r + dv[0][d], c + dv[1][d]
     if 0 <= ax < n and 0 <= ay < n:
         board[ax][ay] += rest
         return out
