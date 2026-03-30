b87a63e492360e74261ffee5ce672bfc083b9849
sani0928 <kksan12@gmail.com>
2026-03-30 09:13:16 +0900

    [Gold V] Title: 0 만들기, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,26 +1,6 @@
-def calcul(s):
-    res = 0
-    opt = 1 # +는 1, -는 -1
-    i = 0
-    while i < len(s):
-        if s[i] == '+':
-            opt = 1
-        elif s[i] == '-':
-            opt = -1
-        else:
-            num = 0
-            while i < len(s) and (s[i].isdigit() or s[i] == ' '):
-                if s[i].isdigit():
-                    num = num * 10 + int(s[i])
-                i += 1
-            res += opt * num
-            continue
-        i += 1
-    return res == 0
-
 def recur(idx, s):
     if idx == n:
-        if calcul(s):
+        if eval(s.replace(' ', '')) == 0:
             print(s)
         return
     recur(idx + 1, s + ' ' + str(nums[idx]))
