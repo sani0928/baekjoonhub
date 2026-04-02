e59b831e67fc6a0d39af4e51bf43cb63f1af2abf
sani0928 <kksan12@gmail.com>
2026-04-02 21:15:33 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,30 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-data = list(map(str, input().split()))
+def convert(x, y):
+    if type(x) == int:
+        return chr(x + 65) + str(abs(int(y) - 8))
+    return ord(x) - 65, abs(int(y) - 8)
+
+data = list(input().split())
 n = int(data[2])
 data2 = [input().rstrip() for _ in range(n)]
-king = [abs(int(data[0][1]) - 8), ord(data[0][0]) - 65]
-rock = [abs(int(data[1][1]) - 8), ord(data[1][0]) - 65]
+king = list(convert(data[0][0], data[0][1]))
+rock = list(convert(data[1][0], data[1][1]))
 
 cmd = ['R', 'L', 'B', 'T', 'RT', 'LT', 'RB', 'LB']
 dv = [
-    (0, 1), (0, -1), (1, 0), (-1, 0),
-    (-1, 1), (-1, -1), (1, 1), (1, -1)
+    (1, 0), (-1, 0), (0, 1), (0, -1),
+    (1, -1), (-1, -1), (1, 1), (-1, 1)
 ]
 for c in data2:
     d = cmd.index(c)
-    nr, nc = king[0] + dv[d][0], king[1] + dv[d][1]
-    if 0 > nr or 0 > nc or 8 <= nr or 8 <= nc:
+    nx, ny = king[0] + dv[d][0], king[1] + dv[d][1]
+    if 0 > nx or 0 > ny or 8 <= nx or 8 <= ny:
         continue
-    if nr == rock[0] and nc == rock[1]:
-        nr2, nc2 = rock[0] + dv[d][0], rock[1] + dv[d][1]
-        if 0 > nr2 or 0 > nc2 or 8 <= nr2 or 8 <= nc2:
+    if nx == rock[0] and ny == rock[1]:
+        nr2, ny2 = rock[0] + dv[d][0], rock[1] + dv[d][1]
+        if 0 > nr2 or 0 > ny2 or 8 <= nr2 or 8 <= ny2:
             continue
-        king[0], king[1] = nr, nc
-        rock[0], rock[1] = nr2, nc2
+        king[0], king[1] = nx, ny
+        rock[0], rock[1] = nr2, ny2
         continue
-    king[0], king[1] = nr, nc
+    king[0], king[1] = nx, ny
 
-print(chr(king[1] + 65) + str(abs(king[0] - 8)))
-print(chr(rock[1] + 65) + str(abs(rock[0] - 8)))
+print(convert(king[0], king[1]))
+print(convert(rock[0], rock[1]))
