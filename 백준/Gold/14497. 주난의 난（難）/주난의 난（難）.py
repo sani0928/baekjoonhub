71dfeeceefecfd2777615944e78cb1c42e688e67
sani0928 <kksan12@gmail.com>
2026-04-04 20:06:55 +0900

    [Gold IV] Title: 주난의 난(難), Time: 208 ms, Memory: 33828 KB -BaekjoonHub

---
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+dv = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+n, m = map(int, input().split())
+x1, y1, x2, y2 = map(int, input().split())
+x1, y1, x2, y2 = x1 - 1, y1 - 1, x2 - 1, y2 - 1
+board = [list(input().rstrip()) for _ in range(n)]
+
+s = [(x1, y1)]
+vis = [[0] * m for _ in range(n)]
+vis[x1][y1] = 1
+
+ans, end = 0, 0
+while not end:
+    nx = []
+    while s:
+        cr, cc = s.pop()
+        for dr, dc in dv:
+            nr, nc = cr + dr, cc+ dc
+            if 0 > nr or 0 > nc or n <= nr or m <= nc or vis[nr][nc]:
+                continue
+            if nr == x2 and nc == y2:
+                end = 1
+                break
+            vis[nr][nc] = 1
+            if board[nr][nc] == '1':
+                nx.append((nr, nc))
+                continue
+            s.append((nr, nc))
+    s = nx
+    ans += 1
+
+print(ans)
