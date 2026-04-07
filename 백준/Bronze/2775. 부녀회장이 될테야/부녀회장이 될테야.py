64687f6349ca2f1f0f5f7fc2d26d927ecee08173
sani0928 <kksan12@gmail.com>
2026-04-07 20:14:40 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -3,14 +3,17 @@ mx_h, mx_w = 0, 0
 data = []
 for _ in range(T):
     k, n = int(input()), int(input())
-    mx_h, mx_w = max(mx_h, k), max(mx_w, n)
+    mx_h, mx_w = max(mx_h, k + 1), max(mx_w, n)
     data.append((k, n))
-mx_h += 1
 dp = [[0] * mx_w for _ in range(mx_h)]
-for i in range(mx_w):
-    dp[0][i] = i + 1
-for i in range(1, mx_h):
-    for j in range(mx_w ):
+for i in range(mx_h):
+    for j in range(mx_w):
+        if i == 0:
+            dp[i][j] = j + 1
+            continue
+        if j == 0:
+            dp[i][0] = 1
+            continue
         dp[i][j] = dp[i][j-1] + dp[i-1][j]
 for k, n in data:
-    print(dp[k][n-1])
+    print(dp[k][n - 1])
