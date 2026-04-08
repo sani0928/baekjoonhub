282f260be53b664f15fd6c2b17e5b708b298b2a4
sani0928 <kksan12@gmail.com>
2026-04-08 20:06:07 +0900

    [Silver I] Title: 경로 찾기, Time: 212 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,15 +1,9 @@
 n = int(input())
-graph = [tuple(map(int, input().split())) for _ in range(n)]
-vis = [[0] * n for _ in range(n)]
-s = [(node, node) for node in range(n)]
-while s:
-    start, cur = s.pop()
-    for nx, i in enumerate(graph[cur]):
-        if not i:
-            continue
-        if vis[start][nx]:
-            continue
-        vis[start][nx] = 1
-        s.append((start, nx))
-for row in vis:
+graph = [list(map(int, input().split())) for _ in range(n)]
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+for row in graph:
     print(*row)
