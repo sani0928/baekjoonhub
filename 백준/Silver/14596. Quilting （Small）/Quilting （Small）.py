691874e13a3260e3bd3cdb39cff5c4daad1a7653
sani0928 <kksan12@gmail.com>
2026-04-20 06:12:04 +0900

    [Silver I] Title: Quilting (Small), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
+++ 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
@@ -1,14 +1,19 @@
 def calcul(x, y):
     return abs(b[x][y] - b[x+h][y]) ** 2
 
+ans = 10**9
 h, w = map(int, input().split())
-b = [tuple(map(int, input().split())) for _ in range(h*2)]
-mn = [[float('inf')] * w for _ in range(h)]
+b = [list(map(int, input().split())) for _ in range(h*2)]
+s = []
 for i in range(w):
-    mn[0][i] = calcul(0, i)
-for i in range(1, h):
-    for j in range(w):
-        value = calcul(i, j)
-        for k in range(max(0, j - 1), min(w, j + 2)):
-            mn[i][j] = min(mn[i][j], mn[i-1][k] + value)
-print(min(mn[h-1]))
+    s.append((calcul(0, i), 0, i))
+while s:
+    cv, cr, cc = s.pop()
+    if cv >= ans:
+        continue
+    if cr == h - 1:
+        ans = min(ans, cv)
+        continue
+    for nc in range(max(0, cc - 1), min(w, cc + 2)):
+        s.append((cv + calcul(cr + 1, nc), cr + 1, nc))
+print(ans)
