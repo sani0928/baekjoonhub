10f6c9e875dc835de51e9000928ec476ba0b3c12
sani0928 <kksan12@gmail.com>
2026-04-22 14:18:22 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 304 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -4,29 +4,27 @@ def solve():
     def back(idx, cnt, soldier):
         nonlocal ans
         if cnt == 3:
-            res = game(soldier, 0)
+            res = game(soldier, 0, enemy_cnt)
             ans = max(ans, res)
             return
         for i in range(idx, m):
             back(i + 1, cnt + 1, soldier + [(n, i)])
 
-    def game(p, kill):
-        matrix = [row[:] for row in board]
-        while True:
+    def game(p, kill, rest):
+        matrix = [l[:] for l in board]
+        while rest != 0:
             target = set()
-            # 타켓 설정
             for x, y in p:
                 q = deque([(x - 1, y, 1)])
                 v = [[0] * m for _ in range(n)]
-                v[x-1][y] = 1
+                v[x - 1][y] = 1
                 while q:
                     cx, cy, foot = q.popleft()
-
+                    if foot > d:
+                        continue
                     if matrix[cx][cy] == 1:
                         target.add((cx, cy))
                         break
-                    if foot == d:
-                        continue
 
                     for dx, dy in (0, -1), (-1, 0), (0, 1):
                         nx, ny = cx + dx, cy + dy
@@ -35,26 +33,29 @@ def solve():
                         v[nx][ny] = 1
                         q.append((nx, ny, foot + 1))
 
-            rest = 0
             new_matrix = [[0] * m for _ in range(n)]
-            for r in range(n - 1, -1, -1):
-                for c in range(m):
-                    # 타켓이라면 제거
-                    if (r, c) in target:
+            for x in range(n - 1, -1, -1):
+                for y in range(m):
+                    if (x, y) in target:
+                        rest -= 1
                         kill += 1
                         continue
-                    # 남은 적들 이동
-                    if matrix[r][c] == 1:
-                        if r + 1 != n:
-                            rest += 1
-                            new_matrix[r + 1][c] = 1
-            if not rest:
-                break
+                    if matrix[x][y] == 1:
+                        if x + 1 == n:
+                            rest -= 1
+                            continue
+                        new_matrix[x + 1][y] = 1
             matrix = new_matrix
         return kill
 
     n, m, d = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(n)]
+    board, enemy_cnt = [], 0
+    for r in range(n):
+        row = list(map(int, input().split()))
+        for c in range(m):
+            if row[c] == 1:
+                enemy_cnt += 1
+        board.append(row)
     ans = 0
     back(0, 0, [])
     print(ans)
