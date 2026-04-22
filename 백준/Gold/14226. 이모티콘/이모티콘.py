4c2e961eaf8d9cbf4501b1d091ae2f704db8a84d
sani0928 <kksan12@gmail.com>
2026-04-22 11:45:39 +0900

    [Gold IV] Title: 이모티콘, Time: 136 ms, Memory: 63816 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,26 +1,26 @@
-from heapq import heappush, heappop
+from collections import deque
 
 s = int(input())
 vis = [[-1] * (s*2) for _ in range(s*2)]
 vis[1][0] = 0
-hq = []
-heappush(hq, (vis[1][0], 1, 0))
-while hq:
-    time, cur, save = heappop(hq)
+q = deque([(1, 0)])
+while q:
+    cur, save = q.popleft()
     if cur == s:
         print(vis[cur][save])
         break
+    time = vis[cur][save]
     # 복사
     if vis[cur][cur] == -1:
         vis[cur][cur] = time + 1
-        heappush(hq, (vis[cur][cur], cur, cur))
+        q.append((cur, cur))
     # 붙여넣기
     nx = cur + save
     if save > 0 and nx < s*2 and vis[nx][save] == -1:
         vis[nx][save] = time + 1
-        heappush(hq, (vis[nx][save], nx, save))
+        q.append((nx, save))
     # 삭제
     nx = cur - 1
     if vis[nx][save] == -1:
         vis[nx][save] = time + 1
-        heappush(hq, (vis[nx][save], nx, save))
+        q.append((nx, save))
