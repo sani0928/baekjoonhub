643c75fdd571197510b5eba916cc3a79a84cc744
sani0928 <kksan12@gmail.com>
2026-04-24 18:46:27 +0900

    [Silver II] Title: 부자가 될 거야!, Time: 180 ms, Memory: 39712 KB -BaekjoonHub

--- 백준/Silver/32335. 부자가 될 거야！/부자가 될 거야！.py
+++ 백준/Silver/32335. 부자가 될 거야！/부자가 될 거야！.py
@@ -1,13 +1,13 @@
 n, m = map(int, input().split())
 s = list(map(int, input().rstrip()))
-for i in range(len(s)):
-    if s[i] == 0:
-        continue
-    diff = 9 - s[i] + 1
-    if m >= diff:
-        m -= diff
-        s[i] = 0
-    if m == 0:
-        break
-s[-1] = (s[-1] + m % 10) % 10
+i = 0
+while i < len(s) and m > 0:
+    if s[i] != 0:
+        diff = 9 - s[i] + 1
+        if m >= diff:
+            m -= diff
+            s[i] = 0
+    i += 1
+if m > 0:
+    s[-1] = (s[-1] + m % 10) % 10
 print(*s, sep='')
