efd68cc555016720757a996010d67ee664660dd7
sani0928 <kksan12@gmail.com>
2026-04-25 17:05:35 +0900

    [Gold III] Title: 중량제한, Time: 316 ms, Memory: 63724 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop; import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+s, e = map(int, input().split())
+dist = [0] * (n + 1)
+dist[s] = 10**9+1
+hq = []
+heappush(hq, (-(10**9+1), s))
+while hq:
+    cur_mxw, cur_x = heappop(hq)
+    cur_mxw = -cur_mxw
+    if cur_x == e:
+        print(dist[e])
+        break
+    for w, nx in graph[cur_x]:
+        new_mxw = min(cur_mxw, w)
+        if dist[nx] < new_mxw:
+            dist[nx] = new_mxw
+            heappush(hq, (-new_mxw, nx))
